a03636a0638d508ad2f4617fc58e720c0def1b51
tianjiaoshanzai <tianjiaoshanzai@gmail.com>
2019-12-04 10:33:19 +0800

    create

---
+++ src/main/java/cn/edu/hfut/net/netschool/NetschoolApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.hfut.net.netschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetschoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetschoolApplication.class, args);
+    }
+
+}
